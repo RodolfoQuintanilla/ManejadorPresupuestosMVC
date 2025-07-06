346263ed69bcc6d643de38f08c16b727de0868d3
Rodolfo <rqm@outlook.es>
2025-07-05 21:53:59 -0600

    v-109

--- ManejoPresupuesto/ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/ManejoPresupuesto/Models/TipoCuenta.cs
@@ -1,36 +1,62 @@
 
 
 using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.Validaciones;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta
+    public class TipoCuenta  //: IValidatableObject
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo del {0} es requerido")]
+        /* [PrimeraLetraMayuscula] */
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Order { get; set; }
+
+        /* 
+        -----------------Ejemplo  de validacion personalizada------------------
+
+         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+         {
+             if (Nombre != null && Nombre.Length > 0)
+             {
+                 var primeraLetra = Nombre[0].ToString();
+
+                 if (primeraLetra != primeraLetra.ToUpper())
+                 {
+                     yield return new ValidationResult("La primera letra debe ser mayuscula", new[] { nameof(Nombre) });  ==> señala el campo necesario
+                 }
+             }
+         } 
+         */
+
+
+        /* 
+
+         ------------------- Puebas de otras validaciones por defecto ------------------------
 
         [Required(ErrorMessage = "El campo del {0} es requerido")] //==> {0} el uso del placeholder sustituya el nombre del campo
         [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "El campo del {0} debe tener un minimo de {2} y un maximo de {1}")]
         [Display(Name = "Nombre de tipo de cuenta")] //==> Modifica el nombre del formulario con el que esta sincronizado el campo
         public string Nombre { get; set; }
-        public int UsuarioId { get; set; }
-        public int Order { get; set; }
 
-        /*  Puebas de otras validaciones por defecto */
 
-        [Required(ErrorMessage = "El campo del {0} es requerido")]
-        [EmailAddress(ErrorMessage = "El campo del {0} debe ser un email")]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+                [Required(ErrorMessage = "El campo del {0} es requerido")]
+                [EmailAddress(ErrorMessage = "El campo del {0} debe ser un email")]
+                [Display(Name = "Email")]
+                public string Email { get; set; }
 
-        [Range(minimum: 18, maximum: 100, ErrorMessage = "El campo del {0} debe estar entre {1} y {2}")]
-        public int Edad { get; set; }
+                [Range(minimum: 18, maximum: 100, ErrorMessage = "El campo del {0} debe estar entre {1} y {2}")]
+                public int Edad { get; set; }
 
-        [Url(ErrorMessage = "El campo del {0} debe ser una url")]
-        [Display(Name = "UrlXXX")]
-        public string Url { get; set; }
+                [Url(ErrorMessage = "El campo del {0} debe ser una url")]
+                [Display(Name = "UrlXXX")]
+                public string Url { get; set; }
 
-        [CreditCard(ErrorMessage = "La tc no es valida")]
-        [Display(Name = "Tarjeta de Credito")]
-        public string TarjetaDeCredito { get; set; }
+                [CreditCard(ErrorMessage = "La tc no es valida")]
+                [Display(Name = "Tarjeta de Credito")]
+                public string TarjetaDeCredito { get; set; } */
     }
 }
--- ManejoPresupuesto/ManejoPresupuesto/obj/Debug/net9.0/ManejoPresupuesto.AssemblyInfo.cs
+++ ManejoPresupuesto/ManejoPresupuesto/obj/Debug/net9.0/ManejoPresupuesto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c822898839d8d7594beb6cfeaf8e5422819bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0647cd8e0e0154145a7a08a5a623921e50469d4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
